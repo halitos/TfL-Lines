eb315f346497171bc42182d6891cb98a1a3a4c8d
Halit <60817499+halitos@users.noreply.github.com>
2020-09-14 16:51:32 +0100

    up

--- src/Componenets/Journey.js
+++ src/Componenets/Journey.js
@@ -13,28 +13,17 @@ const Journey = ({ line }) => {
   if (lineInfo) {
     return (
       <div>
-        <h2 className="text-sm mb-4 font-bold uppercase">
+        <h2>
           {lineInfo.modeName}: {lineInfo.name}
         </h2>
-        <div className="w-full flex justify-between">
-          <div className="flex flex-col bg-blue-700 rounded-md h-32 shadow-lg border-gray-400 flex-1 mr-3">
-            <h3 className="uppercase text-xs font-bold px-1 pt-1 text-white">
-              Start of Line
-            </h3>
-            <div className="h-full flex items-center justify-center px-3 text-center text-white">
-              {lineInfo.routeSections[0].originationName}
-            </div>
+        <div>
+          <div>
+            <h3>Start of Line</h3>
+            <div>{lineInfo.routeSections[0].originationName}</div>
           </div>
-          {/* <div className="flex justify-center w-6">
-            <Arrow className="w-full" />
-          </div> */}
-          <div className="flex flex-col bg-blue-700 rounded-md h-32 shadow-lg border-gray-400 flex-1 ml-3">
-            <h3 className="uppercase text-xs font-bold px-1 pt-1 text-white">
-              End of Line
-            </h3>
-            <div className="h-full flex items-center justify-center px-3 text-center text-white">
-              {lineInfo.routeSections[0].destinationName}
-            </div>
+          <div>
+            <h3>End of Line</h3>
+            <div>{lineInfo.routeSections[0].destinationName}</div>
           </div>
         </div>
       </div>
